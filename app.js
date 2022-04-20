badb9e4bf1f534351da58d64169c4efc50c895bf
davidlao <61684228+davidlao27@users.noreply.github.com>
2022-04-20 19:08:38 +0200

    Clientside :D :+1:
    Finally, clientside basics are done.
    - Added toggle to draw  or just drag around to test (+ server URL you can change)
    - Drawing with mouse and touch added
    
    1.1 challenges:
    - Mirror the screen for it to be a better "drawing tablet"
    
    SERVERSIDE COMING SOON; although you can make your own right now if you look at the code - but I'd recommend to wait for it.
    
    NOTE: Releases can sometimes come with issues which are fixed later on minor releases when detected after testing.

---
+++ app.js
@@ -0,0 +1,86 @@
+let surl = document.getElementById("serverurl");
+let serverurl = surl.value;
+let socket = new WebSocket(serverurl);
+let di = document.getElementById("status");
+let mousedata;
+let touchdata = 0;
+let canDraw = false;
+
+function log(msg) {
+  di.innerHTML = msg;
+}
+
+function countTouches(event) {
+  var x = event.touches.length;
+  if (x > 0) {
+    if (canDraw) {
+      touchdata = "1"
+    }
+    else { touchdata = "0" }
+  } else {
+    touchdata = "0"
+  }
+
+  mousedata = event.touches[0].clientX + "," + event.touches[0].clientY + ",";
+  
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(mousedata + touchdata);
+  }
+}
+
+function toggleDraw() {
+  if (canDraw == false) canDraw = true
+  else canDraw = false
+}
+
+function initSock() {
+  socket = new WebSocket(serverurl);
+
+  socket.onopen = function (e) {
+    log("Connected to server.")
+    surl.style.position = "absolute";
+    surl.style.visibility = "hidden";
+  };
+
+  socket.onmessage = function (event) {
+    log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      log(`[close] Connection closed cleanly! Refresh the page.`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      log('[close] Connection died. Retrying in 2 seconds... (try to change the URL?)');
+      surl.style.position = "relative";
+      surl.style.visibility = "visible";
+
+      setTimeout(() => {
+        initSock();
+      }, 2000);
+    }
+  };
+
+  socket.onerror = function (error) {
+    log(`[error] ${error.message}`);
+  };
+}
+
+document.addEventListener('mousemove', (event) => {
+  mousedata = event.clientX + "," + event.clientY + ","
+  
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(mousedata + touchdata);
+  }
+});
+
+document.addEventListener('mousedown', (event) => {
+  touchdata = "1"
+});
+
+document.addEventListener('mouseup', (event) => {
+  touchdata = "0"
+});
+
+initSock();
